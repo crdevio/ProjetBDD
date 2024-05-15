76b8472d52eeb332aa08c73ce15322f4e2800a4b
crdevio <clement.rouvroy80@gmail.com>
2024-05-15 10:42:19 +0200

    init commit

---
+++ skeleton/model.py
@@ -0,0 +1,271 @@
+#!/usr/bin/python
+
+import sys
+import psycopg2
+import psycopg2.extras
+
+
+class Model:
+    def __init__(self):
+        self.connection = psycopg2.connect("dbname='TODO' user='TODO' host='psql.eleves.ens.fr' password='TODO'")
+        self.connection.autocommit = True
+        self.cursor = self.connection.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if (self.connection):
+            self.connection.close()
+
+##############################################
+######      Queries for tab  PERSONS    ######
+##############################################
+
+    # Create a new person.
+    def createPerson(self, lastname, firstname, address, phone):
+        self.cursor.execute("""
+        TODO01
+        """, (lastname, firstname, address, phone))
+        self.connection.commit()
+
+    # Return a list of (id, lastname, firstname, address, phone,
+    # number of curriculums) corresponding to all persons.
+    def listPersons(self):
+        self.cursor.execute("""
+        TODO02
+        """)
+        return self.cursor.fetchall()
+
+    # Delete a person given its ID (beware of the foreign constraints!).
+    def deletePerson(self, idPerson):
+        self.cursor.execute("""
+        TODO03
+        """, idPerson)
+        self.connection.commit()
+
+##############################################
+######     Queries for  CURRICULUMS     ######
+##############################################
+
+    # Create a curriculum.
+    def createCurriculum(self, name, secretary, director):
+        self.cursor.execute("""
+        TODO04
+        """, (name, secretary, director))
+        self.connection.commit()
+
+    # Return a list of (id,name of curriculum,director lastname,
+    # director firstname, secretary lastname, secretary firstname)
+    # corresponding to all curriculums.
+    def listCurriculums(self):
+        self.cursor.execute("""
+        TODO05
+        """)
+        return self.cursor.fetchall()
+
+    # Delete a curriculum given its ID (beware of the foreign constraints!).
+    def deleteCurriculum(self, idCurriculum):
+        self.cursor.execute("""
+        TODO06
+        """, idCurriculum)
+        self.connection.commit()
+
+##############################################
+######     Queries for  COURSES         ######
+##############################################
+
+    # Create a course.
+    def createCourse(self, name, idProfessor):
+        self.cursor.execute("""
+        TODO07
+        """, (name, idProfessor))
+        self.connection.commit()
+
+    # Return a list of (course id, course name, teacher id,
+    # teacher last name, teacher first name) corresponding
+    # to all the courses.
+    def listCourses(self):
+        self.cursor.execute("""
+        TODO08
+        """)
+        return self.cursor.fetchall()
+
+    # Delete a given course (beware that the course might be registered to
+    # curriculum, and have grades that should also be deleted).
+    def deleteCourse(self, idCourse):
+        self.cursor.execute("""
+        TODO09
+        """, idCourse)
+        self.connection.commit()
+
+
+##############################################
+###### Queries for tab  CURRICULUM/<ID> ######
+##############################################
+
+    # Get the name of a given curriculum.
+    def getNameOfCurriculum(self, id):
+        self.cursor.execute("""
+        TODO10
+        """, id)
+        # suppose that there is a solution
+        return self.cursor.fetchall()[0][0]
+
+    # Return the list (course ID, course name, course teacher
+    # last name and first name, ECTS) corresponding to the courses
+    # registered to a given curriculum.
+    def listCoursesOfCurriculum(self, idCurriculum):
+        self.cursor.execute("""
+        TODO11
+        """, (idCurriculum))
+        return self.cursor.fetchall()
+
+    #  !! HARD !!
+    # Return a list (last name, first name, average grade) of students
+    # registered to a given curriculum. The
+    # average grade is computed as described in the document, but
+    # beware that if a student does not have a grade for a validation
+    # or is not registered to a course, he should have 0.
+    def averageGradesOfStudentsInCurriculum(self, idCurriculum):
+        self.cursor.execute("""
+        TODO12
+        """, idCurriculum)
+        return self.cursor.fetchall()
+
+    # Register a person to a curriculum.
+    def registerPersonToCurriculum(self, idPerson, idCurriculum):
+        self.cursor.execute("""
+        TODO13
+        """, (idPerson, idCurriculum))
+        self.connection.commit()
+
+    # Register a course to a curriculum.
+    def registerCourseToCurriculum(self, idCourse, idCurriculum, ects):
+        self.cursor.execute("""
+        TODO14
+        """, (idCourse, idCurriculum, ects))
+        self.connection.commit()
+
+    # Unregister a course to a curriculum.
+    def deleteCourseFromCurriculum(self, idCourse, idCurriculum):
+        self.cursor.execute("""
+        TODO15
+        """, (idCurriculum, idCourse))
+        self.connection.commit()
+
+##############################################
+######   Queries for tab  COURSE/<ID>   ######
+##############################################
+
+    # Get the name of a given course.
+    def getNameOfCourse(self, id):
+        self.cursor.execute("""
+        TODO16
+        """, id)
+        # suppose that there is a solution
+        return self.cursor.fetchall()[0][0]
+
+    # Return a list of (id, name, ECTS) of the curriculums in
+    # which a given course is registered.
+    def listCurriculumsOfCourse(self, idCourse):
+        self.cursor.execute("""
+        TODO17
+        """, (idCourse))
+        return self.cursor.fetchall()
+
+    # Returns a list of (id, date, name, coefficent) for the validations
+    # assiociated to a given course.
+    def listValidationsOfCourse(self, idCourse):
+        self.cursor.execute("""
+        TODO18
+        """, idCourse)
+        return self.cursor.fetchall()
+
+    # Return a list (id, last name, first name) of persons that are
+    # registered in a curriculum with the given course
+    def listStudentsOfCourse(self, idCourse):
+        self.cursor.execute("""
+        TODO19
+        """, idCourse)
+        return self.cursor.fetchall()
+
+    # Return a list (id, date, curriculum name, student last name,
+    # student first name, validation name, grade, coefficient) of
+    # grades for all the validations and students having taken them,
+    # sorted by decreasing date of validation.
+    def listGradesOfCourse(self, idCourse):
+        self.cursor.execute("""
+        TODO20
+        """, idCourse)
+        return self.cursor.fetchall()
+
+    # Add a validation to a given course.
+    def addValidationToCourse(self, name, coef, date, idCourse):
+        self.cursor.execute("""
+        TODO21
+        """, (name, coef, date, idCourse))
+        self.connection.commit()
+
+    # Add a grade to a student.
+    def addGrade(self, idValidation, idStudent, grade):
+        self.cursor.execute("""
+        TODO22
+        """, (idValidation, idStudent, grade))
+        self.connection.commit()
+
+##############################################
+######       Queries for tab            ######
+######      COURSE/<ID1>/<ID2           ######
+###### corresponding to validations     ######
+##############################################
+
+   # Return a list (grade, lastname, firstname) of grades for
+   # a given validation.
+    def listGradesOfValidation(self, idValidation):
+        self.cursor.execute("""
+        TODO23
+        """, idValidation)
+        return self.cursor.fetchall()
+
+    # Get the complete name of a validation given its ID. The
+    # complete name of a validation with name "exam" of a course "BDD"
+    # is "BDD - exam". You should therefore preppend the name of the
+    # course.
+    def getNameOfValidation(self, id):
+        self.cursor.execute("""
+        TODO24
+        """, id)
+        # suppose that there is a solution
+        return self.cursor.fetchall()[0][0]
+
+##############################################
+######   Queries for tab  PERSON/<ID>   ######
+##############################################
+
+    # Get the name of a person given its ID.
+    def getNameOfPerson(self, id):
+        self.cursor.execute("""
+        TODO25
+        """, id)
+        # suppose that there is a solution
+        return self.cursor.fetchall()[0][0]
+
+    # Return a list (id, date, curriculum name, course name,
+    # exam name, grade) of grades for a given student, sorted
+    # by decreasing date of validation.
+    def listValidationsOfStudent(self, idStudent):
+        self.cursor.execute("""
+        TODO26
+        """, idStudent)
+        return self.cursor.fetchall()
+
+    # !!! HARD !!!
+    # Return a list (curriculum name, average grade) of all the
+    # curriculum a given student is registered to, where the
+    # average grade is computed as before.
+    def listCurriculumsOfStudent(self, idStudent):
+        self.cursor.execute("""
+        TODO27
+        """, (idStudent, idStudent))
+        return self.cursor.fetchall()
